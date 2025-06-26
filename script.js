45fb2b63c68c81b90c4c2bc19679924330c1faa3
S Tabaku <silva.tabaku66l@gmail.com>
2025-06-26 18:00:27 +0200

    Hero and text

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const mobileMenu = document.getElementById('mobileMenu');
 hamburger.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
+
+const indicators = document.querySelectorAll('.indicator');
+
+indicators.forEach(indicator => {
+  indicator.addEventListener('click', () => {
+    indicators.forEach(ind => ind.classList.remove('active'));
+    indicator.classList.add('active');
+  });
+});
